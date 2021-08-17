d7b491830b7cc9a1d086ac58e50070f03f0d5022
Marcin Rudolf <rudolfix@rudolfix.org>
2021-08-17 21:26:05 +0200

    adds test utils with constant and deployers

---
+++ test/utils/consts.js
@@ -0,0 +1,4 @@
+const { BN } = require('@openzeppelin/test-helpers');
+
+export const Q18 = (new BN("10")).pow(new BN("18"));
+export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
