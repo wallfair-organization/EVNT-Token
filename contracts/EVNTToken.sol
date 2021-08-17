87bba5dfe1f46820f8a71aebb8909a2719b048d4
Marcin Rudolf <rudolfix@rudolfix.org>
2021-08-17 21:25:00 +0200

    passes token allocation directly to constructor in EVNTToken

--- contracts/EVNTToken.sol
+++ contracts/EVNTToken.sol
@@ -4,27 +4,12 @@ pragma solidity ^0.8.4;
 
 import "erc-payable-token/contracts/token/ERC1363/ERC1363.sol";
 
-
 contract EVNTToken is ERC1363 {
-    address creator;
-
-    constructor()
-        ERC20("EVNT. Token", "EVNT") {
-        creator = msg.sender;
-    }
-
-    function mint(uint256 amount) public isCreator returns (bool) {
-        require(
-            totalSupply() + amount <= 1000000000 * 10**decimals(),
-            "You can't mint more then 400.000.000 EVNT"
-        );
-
-        _mint(_msgSender(), amount);
-        return true;
-    }
-
-    modifier isCreator() {
-        require(creator == msg.sender, "The caller is not the creator!");
-        _;
+    // create EVNT token instance and immediately mint all tokens, no further minting will be possible
+    constructor(address[] memory wallets, uint256[] memory amounts) ERC20("EVNT Token", "EVNT") {
+        require(wallets.length == amounts.length, "no. elements in lists must match");
+        for (uint256 ii = 0; ii < wallets.length; ii += 1) {
+            _mint(wallets[ii], amounts[ii]);
+        }
     }
 }
