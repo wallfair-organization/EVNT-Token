237539937ff2437f8010c8456099d0c292cef00c
Keir Finlow-Bates <keir@chainfrog.com>
2021-08-18 07:48:01 +0300

    block timestamp with no cliff test

--- test/TokenLock-views.test.js
+++ test/TokenLock-views.test.js
@@ -48,52 +48,52 @@ contract('TestTokenLock', function (accounts) {
 
   // Basic view tests
 
-  it('Check token address is correct', async () => {
+  it('check token address is correct', async () => {
     const testTokenLock = await TestTokenLock.deployed();
     const wallfairToken = await WallfairToken.deployed();
     expect(await testTokenLock.token()).to.equal(wallfairToken.address); 
   });
 
-  it('Check startTime is as deployed', async () => {
+  it('check startTime is as deployed', async () => {
     const testTokenLock = await TestTokenLock.deployed(); 
     expect(await testTokenLock.startTime()).to.be.a.bignumber.to.equal(new BN('1612137600'));
   });
 
-  it('Check vestingPeriod is as deployed', async () => {
+  it('check vestingPeriod is as deployed', async () => {
     const testTokenLock = await TestTokenLock.deployed();
     expect(await testTokenLock.vestingPeriod()).to.be.a.bignumber.to.equal(new BN((4 * 365 * 24 * 60 * 60).toString()));
   });
 
-  it('Check cliffPeriod is as deployed', async () => {
+  it('check cliffPeriod is as deployed', async () => {
     const testTokenLock = await TestTokenLock.deployed();
     expect(await testTokenLock.cliffPeriod()).to.be.a.bignumber.to.equal(new BN((1 * 365 * 24 * 60 * 60).toString()));
   });
 
-  it('Check totalTokensOf are as deployed', async () => {
+  it('check totalTokensOf are as deployed', async () => {
     const testTokenLock = await TestTokenLock.deployed();
     expect(await testTokenLock.totalTokensOf(stakedAccountID)).to.be.a.bignumber.to.equal((LOCK_AMOUNT).toString());
   });
 
-  it('Check the initial vesting is 0', async () => {
+  it('check the initial vesting is 0', async () => {
     const testTokenLock = await TestTokenLock.deployed();
     expect(await testTokenLock.tokensVested(stakedAccountID, 1612137600, { from: stakedAccountID })).to.be.a.bignumber.to.equal(new BN('0'));
   });
 
-  it('Check there are no unlocked tokens at the initial deployment', async () => {
+  it('check there are no unlocked tokens at the initial deployment', async () => {
     const testTokenLock = await TestTokenLock.deployed();
     expect((await testTokenLock.unlockedTokensOf(stakedAccountID, { from: stakedAccountID })).toString()).to.equal('0');
   });
 
-  it('Check cliff period has not been exceeded for TokenLock contract', async () => {
+  it('check cliff period has not been exceeded for TokenLock contract', async () => {
     const testTokenLock = await TestTokenLock.deployed();
     block = await ethers.provider.getBlock('latest');
     expect(await testTokenLock.cliffExceeded(block.timestamp)).to.be.a.bignumber.to.equal(new BN('0'));
   });
 
-  it('Check cliff period has been exceeded for TokenLockNoCliff contract', async () => {
+  it('check cliff period has been exceeded for TokenLockNoCliff contract', async () => {
     const testTokenLockNoCliff = await TestTokenLockNoCliff.deployed();
     block = await ethers.provider.getBlock('latest');
-    expect(await testTokenLock.cliffExceeded(block.timestamp)).to.be.a.bignumber.to.equal(new BN('1'));
+    expect(await testTokenLockNoCliff.cliffExceeded(block.timestamp)).to.be.a.bignumber.to.equal(new BN('1'));
   });
 
   // tokensVested takes an address and a timestamp as an input, so is a bit more complicated
