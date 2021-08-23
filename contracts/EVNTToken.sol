83b587986d83ffb5fc33551c629ca60b8c67d3b2
Marcin Rudolf <rudolfix@rudolfix.org>
2021-08-23 21:08:00 +0200

    bumps solidity + some small contract tweaks

--- contracts/EVNTToken.sol
+++ contracts/EVNTToken.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.7;
 
 import "erc-payable-token/contracts/token/ERC1363/ERC1363.sol";
 
 contract EVNTToken is ERC1363 {
     // create EVNT token instance and immediately mint all tokens, no further minting will be possible
-    constructor(uint256 totalSupply) ERC20("EVNT Token", "EVNT") {
-        _mint(msg.sender, totalSupply);
+    constructor(uint256 totalSupply_) ERC20("EVNT Token", "EVNT") {
+        _mint(msg.sender, totalSupply_);
     }
 }
