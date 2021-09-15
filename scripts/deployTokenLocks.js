ff5dbc7778361031cc01ded6d5afcb53664f3de8
kf106 <keir@chainfrog.com>
2021-09-15 14:07:41 +0300

    Update token deployment to use initialFraction, remove secondary transfers

--- scripts/deployTokenLocks.js
+++ scripts/deployTokenLocks.js
@@ -1,5 +1,4 @@
 /* ./scripts/deployTokenLock.js */
-import { transfers } from './utils/transfers';
 require('log-timestamp');
 const hre = require('hardhat');
 const toBN = hre.ethers.BigNumber.from;
@@ -25,7 +24,6 @@ function multipleGroupByArray (dataArray, groupPropertyArray) {
 const network = hre.hardhatArguments.network;
 console.log('Operating on network: ' + network);
 const actionsFilepath = './scripts/' + network + '/logs/actions.json';
-
 let lockConfig;
 try {
   lockConfig = JSON.parse(fs.readFileSync('./scripts/' + network + '/deployTokenLock.config.json', 'utf-8'));
@@ -33,6 +31,8 @@ try {
   console.error(err);
   process.exit(1);
 }
+// Calculate Unix timestamp from UTC datetime
+const TGE_TIME = Math.floor(new Date(lockConfig.TGETime).getTime() / 1000);
 
 // Load actions object (TODO: move actions function to utils)
 let actions;
@@ -49,10 +49,10 @@ try {
   }
 };
 console.log('Actions will be logged to ' + actionsFilepath);
-// Retrieve WFAIR contract address
+
+// Retrieve WFAIR contract address from config file
 const WFAIR_CONTRACT = actions.token.address;
-// Add transfers key if it doesn't exist
-if (!('transfers' in actions)) { actions.transfers = []; };
+
 // Add locks key if it doesn't exist
 if (!('locks' in actions)) { actions.locks = []; };
 
@@ -60,7 +60,7 @@ if (!('locks' in actions)) { actions.locks = []; };
 // can be deployed to the same lock contract due to identical startDate, vesting period, cliff, and initial
 // payout values
 const lockGroups = multipleGroupByArray(lockConfig.lockRequests, function (item) {
-  return [item.startTime, item.vestingPeriod, item.cliffPeriod, item.initialReleaseFraction, item.delay];
+  return [item.vestingPeriod, item.cliffPeriod, item.initialReleaseFraction, item.delay];
 });
 console.log('Number of lock functions to deploy: ' + lockGroups.length);
 console.log(lockGroups);
@@ -80,10 +80,6 @@ for (const lockRequest of lockConfig.lockRequests) {
     process.exit(1);
   }
   TOTAL = TOTAL.add(toBN(Q18.mul(lockRequest.amount)));
-}
-// increase TOTAL to include quantities from initial release transactions
-for (const transferRequest of lockConfig.transferRequests) {
-  TOTAL = TOTAL.add(toBN(Q18.mul(transferRequest.amount)));
 }
 console.log('WFAIR to be locked: ' + TOTAL.div(Q18));
 
@@ -105,21 +101,6 @@ async function main () {
     console.log('ETH balance of ' + ethBalance + ' is sufficient for gas');
   }
 
-  // Check WFAIR balance of deployer is sufficient
-  const WfairToken = await hre.ethers.getContractFactory('WFAIRToken');
-  const wfairtoken = WfairToken.attach(WFAIR_CONTRACT);
-  console.log('Attached to WFAIR token contract ' + WFAIR_CONTRACT);
-  const wfairBalance = await wfairtoken.balanceOf(accounts[0].address);
-  if (wfairBalance.lt(TOTAL)) {
-    console.error('Error: WFAIR balance of deploying address is ' + wfairBalance.div(Q18) +
-       ' but ' + TOTAL.div(Q18) + ' is required');
-    process.exit(1);
-  } else {
-    // TODO: add second comparison and exit on too many tokens
-    const excess = wfairBalance.sub(TOTAL);
-    console.log('Excess WFAIR tokens is: ' + excess.div(Q18));
-  }
-
   // loop through each array of token locks and deploy
   for (const lockGroup of lockGroups) {
     // extract correct arguments array from objects in lockGroup elements
@@ -135,11 +116,10 @@ async function main () {
     console.log('Processing the following lock group:\n', lockGroup);
     const contractParams = [
       WFAIR_CONTRACT,
-      (Math.floor(new Date(lockGroup[0].startTime).getTime() / 1000) +
-        (lockGroup[0].delay * 30 * 24 * 60 * 60)).toString(),
+      (TGE_TIME + (lockGroup[0].delay * 30 * 24 * 60 * 60)).toString(),
       (parseInt(lockGroup[0].vestingPeriod) * 30 * 24 * 60 * 60).toString(),
       (parseInt(lockGroup[0].cliffPeriod) * 30 * 24 * 60 * 60).toString(),
-      lockGroup[0].initialReleaseFraction,
+      (Q18.mul(lockGroup[0].initialReleaseFraction)).toString(),
       wallets,
       amounts,
     ];
@@ -155,22 +135,7 @@ async function main () {
       timestamp: Date.now().toString(),
     };
     actions.locks.push(contractDetails);
-    // and fund the token contract with WFAIR tokens
-    // *** Note: if token contracts are to be funded separately, this small section should be
-    // pulled out into its own script.
-    const transferToLock = {
-      name: 'Fund ' + contractDetails.name,
-      address: contractDetails.address,
-      amount: totalLockFund.toString(),
-    };
-    const result = await transfers(wfairtoken, accounts[0].address, [transferToLock]);
-    actions.transfers.push(...result);
   }
-
-  // loop through transferRequests and send tokens to initial release wallets
-  const result = await transfers(wfairtoken, accounts[0].address, lockConfig.transferRequests);
-  actions.transfers.push(...result);
-  console.log('The following transactions were processed: ', result);
 }
 
 main()
