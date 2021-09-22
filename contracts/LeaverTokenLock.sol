3882cd0da3f5144acbd53db3f2edf2142521237e
Marcin Rudolf <rudolfix@rudolfix.org>
2021-09-22 10:51:01 +0200

    implements first version of bad/good leaver lock

--- contracts/LeaverTokenLock.sol
+++ contracts/LeaverTokenLock.sol
@@ -4,17 +4,30 @@ pragma solidity ^0.8.7;
 
 import "./TokenLock.sol";
 
-
 contract LeaverTokenLock is TokenLock {
     using SafeERC20 for IERC20;
 
-    event LogLeave(address indexed leaver, uint256 remainingTokens);
+    enum LeaverType {
+        None,
+        GoodLeaver,
+        BadLeaver
+    }
+
+    event LogLeave(address indexed leaver, LeaverType leaverType, uint256 newTotalStake);
 
     // manager address that can execute leave method
     address internal _manager;
 
+    // information on leavers
+    mapping(address => LeaverType) internal _leavers;
+
     modifier onlyManager() {
         require(msg.sender == _manager, "Only manager");
+        _;
+    }
+
+    modifier onlyNonLeaver(address wallet) {
+        require(_leavers[wallet] == LeaverType.None, "Specified wallet already left");
         _;
     }
 
@@ -22,44 +35,91 @@ contract LeaverTokenLock is TokenLock {
         IERC20 token_,
         uint256 startTime_,
         uint256 vestingPeriod_,
-        uint256 cliffPeriod_,
-        uint256 initialReleaseFraction_,
+        address manager_,
         address[] memory wallets_,
-        uint128[] memory amounts_,
-        address manager_
-    ) TokenLock(
-        token_,
-        startTime_,
-        vestingPeriod_,
-        cliffPeriod_,
-        initialReleaseFraction_,
-        wallets_,
-        amounts_
-    ) {
+        uint128[] memory amounts_
+    ) TokenLock(token_, startTime_, vestingPeriod_, 0, 0, wallets_, amounts_) {
         _manager = manager_;
     }
 
-    function leave(address wallet) public onlyManager {
+    function managerAddress() public view returns (address) {
+        return _manager;
+    }
+
+    function hasLeaved(address wallet) public view returns (LeaverType) {
+        return _leavers[wallet];
+    }
+
+    function leaveWallet(address wallet, bool isBadLeaver) public onlyManager onlyNonLeaver(wallet) {
+        require(msg.sender != wallet, "Manager cannot leave");
+
         UnlockState memory stake = _stakes[wallet];
         uint256 tokensVestedSoFar = tokensVestedInternal(stake.totalTokens, block.timestamp);
+        uint256 newTotalStake = 0;
+        if (tokensVestedSoFar == 0) {
+            // nothing got released yet - use accumulation function instead
+            uint256 accumulatedSoFar = tokensAccumulatedInternal(stake.totalTokens, block.timestamp);
+            if (isBadLeaver) {
+                // keep only 10%
+                newTotalStake = accumulatedSoFar / 10;
+            } else {
+                // keeps all accumulated + 50% of what remains
+                newTotalStake = accumulatedSoFar + (stake.totalTokens - accumulatedSoFar) / 2;
+            }
+        } else {
+            // leavers keep what was released
+            newTotalStake = tokensVestedSoFar;
+            if (!isBadLeaver) {
+                // 50% of what remains if not bad leaver
+                newTotalStake += tokensVestedSoFar + (stake.totalTokens - tokensVestedSoFar) / 2;
+            }
+        }
+
+        // return unlocked tokens
         uint256 unlockAmount = tokensVestedSoFar - stake.unlockedTokens;
-
-        // this should never happen
-        assert(stake.totalTokens >= stake.unlockedTokens + unlockAmount);
-
         if (unlockAmount > 0) {
+            // this should never happen
+            assert(stake.totalTokens >= stake.unlockedTokens + unlockAmount);
+
+            // if anything was unlocked - book it
+            _stakes[wallet].unlockedTokens += unlockAmount;
+
             // send the original wallet what is now due
             token().safeTransfer(wallet, unlockAmount);
             emit LogRelease(wallet, unlockAmount);
         }
 
-        // move stake to the manager
-        _stakes[msg.sender].totalTokens += stake.totalTokens;
-        _stakes[msg.sender].unlockedTokens += stake.unlockedTokens + unlockAmount;
+        // set new stake for leaver
+        // total stake in the contract must be invariant
+        // we take (stake.totalTokens - newTotalStake) from _stakes[wallet].totalTokens (leaver stake)
+        _stakes[wallet].totalTokens = newTotalStake;
+        // and add it to the manager stake
+        _stakes[msg.sender].totalTokens += (stake.totalTokens - newTotalStake);
 
-        // remove stake from wallet
-        delete _stakes[wallet];
+        // mark wallet left
+        LeaverType leaverType = isBadLeaver ? LeaverType.BadLeaver : LeaverType.GoodLeaver;
+        _leavers[wallet] = leaverType;
 
-        emit LogLeave(wallet, stake.totalTokens - tokensVestedSoFar);
+        emit LogLeave(wallet, leaverType, newTotalStake);
+    }
+
+    function tokensAccumulated(address sender, uint256 timestamp) public view returns (uint256 accumulatedTokens) {
+        return tokensAccumulatedInternal(_stakes[sender].totalTokens, timestamp);
+    }
+
+    function tokensAccumulatedInternal(uint256 totalTokens, uint256 timestamp)
+        internal
+        view
+        returns (uint256 accumulatedTokens)
+    {
+        // accumulation period starts "vestingPeriod" before startTime
+        uint256 accStartTime = _startTime - _vestingPeriod;
+        if (timestamp >= accStartTime) {
+            // and lasts 2 * _vestingPeriod
+            uint256 accPeriod = 2 * _vestingPeriod;
+            uint256 timeAccSoFar = Math.min(timestamp - accStartTime, accPeriod);
+            // mul first for best precision, v.8 compiler reverts on overflows
+            accumulatedTokens = (totalTokens * timeAccSoFar) / accPeriod;
+        }
     }
 }
