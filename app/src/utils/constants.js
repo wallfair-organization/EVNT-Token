6497a5ef0503f9c49250cab902c216f7a43428cf
Marcin Rudolf <rudolfix@rudolfix.org>
2021-10-05 23:25:07 +0200

    removes unnecessary network provider

--- app/src/utils/constants.js
+++ app/src/utils/constants.js
@@ -2,6 +2,5 @@ import { ethers } from "ethers";
 
 export const toBN = ethers.BigNumber.from;
 export const ZERO = toBN("0");
-export const NetworkContextName = 'NETWORK';
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 export const IS_IN_IFRAME = window.parent !== window;
