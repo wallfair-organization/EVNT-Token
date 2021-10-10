d0df74ef2cde1012562f158fee87f74d3d9fe838
Marcin Rudolf <rudolfix@rudolfix.org>
2021-10-10 13:49:04 +0200

    texting changes

--- app/src/components/Web3Status/index.js
+++ app/src/components/Web3Status/index.js
@@ -29,8 +29,8 @@ function Web3StatusInner () {
       <div className={styles.connectWrapper}>
         <strong>Hello WFAIR Investor!</strong>
         <p>
-          Do you have mobile wallet? Then you can view this page directly in there or connect it with the desktop. Try
-          one of those.
+        If you have a mobile wallet, you will be able to view this page directly inside of the wallet interface or connect it with your desktop version. 
+        Please select one of the options below.
         </p>
         <div className={styles.walletAppInfo}>
           <img src={MetaMaskIcon} alt={`MetaMask`} />
